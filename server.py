78164489de71031fcc40d883124821c725e6ec0b
CRoberts245 <61851308+CRoberts245@users.noreply.github.com>
2020-07-16 17:13:16 -0700

    Add files via upload

---
+++ server.py
@@ -0,0 +1,57 @@
+#Cameron Roberts
+#5/6/2020
+#Python Capstone Networking Prof Amamra
+import socket
+from threading import Thread
+
+#tcp info
+TCP_IP = 'localhost'
+TCP_PORT = 10000
+BUFFER_SIZE = 1024
+
+
+#class defining each thread
+class ClientThread(Thread):
+
+    def __init__(self,ip,port,sock):#assign thread self values
+        Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.sock = sock
+        print "  User Thread Info: "+ip+":"+str(port)
+
+    def run(self):#define run parameters
+        filenodeName='mytesting.txt'
+        fStream = open(filenodeName,'rb')
+        while True:
+            l = fStream.read(BUFFER_SIZE)
+            while (l):
+                self.sock.send(l)
+                l = fStream.read(BUFFER_SIZE)
+            if not l:
+                fStream.close()
+                self.sock.close()
+                break
+
+#keep array of threads
+threads = []
+
+while True:
+    #initialize socket
+    tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    tcpsock.bind((TCP_IP, TCP_PORT))
+    tcpsock.listen(5)
+    
+    print "Awaiting Connection...."
+    (conn, (ip,port)) = tcpsock.accept()
+    print ' User Connected'
+    newthread = ClientThread(ip,port,conn)
+    newthread.start()
+    threads.append(newthread)
+    #close socket, so that it can be reused
+    tcpsock.close()
+
+for t in threads:
+    t.join()#join all threads
+
